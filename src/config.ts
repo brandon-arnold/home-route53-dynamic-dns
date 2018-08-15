6a36679ea68ab5ac37c0e7fc88b46f2b07aa52e7
Brandon Arnold <br@ndonarnold.email>
2018-08-14 19:24:26 -0500

    feat(config): add support for hosted zone and record set name

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const cachedIpFilename = process.env.ROUTE_53_DYNDNS_CACHED_IP || '/tmp/route53-dyndns-ip.cache';
-
+export const hostedZoneId = process.env.ROUTE_53_DYNDNS_HOSTED_ZONE_ID || '';
+export const recordSetName = process.env.ROUTE_53_DYNDNS_RECORD_SET_NAME || '';
